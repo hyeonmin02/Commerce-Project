ff753724479dc612228b30907437a54437f267b6
hyeonmin02 <hyenmin5@gmail.com>
2026-01-22 19:16:37 +0900

    새 Exception예외객체 생성,CartItem장바구니 상품,수량 메서드,장바구니생성

---
+++ src/example/MyException.java
@@ -0,0 +1,11 @@
+package example;
+
+public class MyException extends RuntimeException {
+    public MyException() {
+        super();
+    }
+
+    public MyException(String message) {
+        super(message);
+    }
+}
